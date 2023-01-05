c2a799c5c80bb82c91b07e09ab3ffbd818d86b63
kyuwonism <kimroot@kakao.com>
2023-01-05 20:58:15 +0900

    vue-todo first commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
+                <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
+                <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+                <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+                    <i class="fas fa-trash-alt"></i>
+                </span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods: {
+        removeTodo: function(todoItem, index) {
+            this.$emit('removeItem', todoItem, index);
+        },
+        toggleComplete: function(todoItem, index) {
+            this.$emit('toggleItem', todoItem, index);
+            
+        }
+    },
+}
+</script>
+
+<style scoped>
+ul {
+    list-style-type: none;
+    padding-left: 0px;
+    margin-top: 0px;
+    text-align: left;
+}
+
+li {
+    display: flex;
+    min-height: 50px;
+    height: 50px;
+    line-height: 50px;
+    margin: 0.5rem 0;
+    padding: 0 0.9rem;
+    background: white;
+    border-radius: 5px;
+}
+.checkBtn {
+    line-height: 45px;
+    color: #62ACDE;
+    margin-right: 5px;
+}
+.checkBtnCompleted {
+    color: #B3ADAD;
+}
+.textCompleted {
+    text-decoration: line-through;
+    color: #B3ADAD;
+}
+.removeBtn {
+    margin-left: auto;
+    color: #DE4343;
+}
+</style>
