06a961c781edf6fbd9c450b329337a2c24996605
kyuwonism <kimroot@kakao.com>
2023-01-09 17:38:45 +0900

    vue todo App 완료

---
+++ src/components/DemoSite.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="root">
+    <p>{{ originalPrice }}</p>
+    <p>{{ doublePrice }}</p>
+    <p>{{ triplePrice }}}</p>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    originalPrice() {
+      this.$store.getters.originalPrice ;
+    },
+    doublePrice() {
+      this.$store.getters.doublePrice;
+    },
+    triplePrice() {
+      this.$store.getters.triplePrice;
+    }
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import {createStore} from 'vuex';
+import todoApp from './modules/todoApp'
+
+export const store = createStore({
+  modules: {
+    todoApp: todoApp
+  }
+})
